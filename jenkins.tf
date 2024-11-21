6fcd4a9f84b91f42b3616a46d9d0da6ac2ba1a73
jmalcolm-cctb <145724644+jmalcolm-cctb@users.noreply.github.com>
2024-11-20 16:56:08 -0800

    Update jenkins.tf
    Revert script changes

--- jenkins.tf
+++ jenkins.tf
@@ -7,16 +7,14 @@ resource "aws_instance" "app_server" {
   tags = {
      Name = "Jenkins"
   }
-  
-  provisioner "file" {
-     source = "setup-jenkins.sh"
-     destination = "/tmp/setup-jenkins.sh"
-  }
 
   provisioner "remote-exec" {
      inline = [
-        "chmod +x /tmp/setup-jenkins.sh",
-        "/tmp/setup-jenkins.sh"
+        "sudo dnf -y install docker",
+        "sudo service docker start",
+        "sudo docker run -u root --privileged -p 80:8080 --name jenkins-server -d -v /var/run/docker.sock:/var/run/docker.sock -v /home/ec2-user:/var/jenkins jenkins/jenkins:lts",
+        "sudo docker exec jenkins-server apt update",
+        "sudo docker exec jenkins-server apt install -y docker.io"
      ]
    }
 
